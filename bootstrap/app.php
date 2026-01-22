e05c2a33b9e0bbb57d6e122c0e222b6567759c3d
Omar Alfarraj <omar.alfarraj@yahoo.com>
2026-01-22 14:54:48 +0300

    feat: add curl command permission and last error retrieval to settings; update middleware to redirect guests to 404

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->append(SecurityHeaders::class);
+        $middleware->redirectGuestsTo(fn () => abort(404));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
